2b71d0fa98c18082f68c9b4f5bba0f194c66043b
Jeffrey H. Johnson <trnsz@pobox.com>
2024-10-21 03:22:31 -0400

    Create `platform.mk` for platform-specific build configuation
    Apply some automatic heuristics to determine (without the
    need for user interaction) a build configuration to build
    optimally with just `make`, and all without interfering
    cross-compiled builds.
    
    This is done using only GNU Make and POSIX-conforming shell
    utilities (`printf`, `uname`, and `grep`).
    
    - Determine if CC is Clang or GCC (by checking the output of
      `--version`, and if it is, use extra warning flags, defined
      in EXTRA_CFLAGS.
    
    - Attempts to quickly determines if -flto=auto is invalid,
      and if so, uses -flto instead.  This is done by invoking
      the compiler frontend in preprocessor mode and passing
      the flag.  It is not foolproof, but it is fast, it doesn't
      require compiling any actual code and handling the output.
      - If CC contains "icc" (Intel C Compiler Classic), enable IPO.
      - If CC contains "suncc" (Oracle Developer Studio),
        "nvc" (NVIDIA HPC SDK), or "xlc" (IBM XLC <=16), disable LTO.
      - Disables LTO when using GCC on IBM AIX or IBM OS/400.
      - Always use "-flto" for LTO on Solaris or illumos systems.
    
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

---
+++ platform.mk
@@ -0,0 +1,75 @@
+# Determine if CC is GCC or Clang
+TGCLT:=$(shell $(CC) --version 2>&1 | grep -q -i -e 'clang' -e 'gcc' && \
+         printf '%s\n' "1" || printf '%s\n' "0")
+ifeq "$(TGCLT)" "1"
+ GCC_CL=1
+endif
+
+# If CC is GCC or Clang, use extra warning flags
+EXTRA_CFLAGS := -Wall -Werror=implicit-function-declaration -Werror=int-conversion
+ifeq "$(GCC_CL)" "1"
+ CFLAGS += $(EXTRA_CFLAGS)
+endif
+
+# Determine LTO invocation: Try `-flto=auto`, fall back to `-ftlo`
+LTOSYN:=$(shell $(CC) -flto=auto -E - < /dev/null > /dev/null 2>&1 && \
+         printf '%s\n' "-flto=auto" || printf '%s\n' "-flto")
+ifeq "$(findstring xlc,$(CC))" ""
+ ifeq "$(findstring nvc,$(CC))" ""
+  ifeq "$(findstring suncc,$(CC))" ""
+   ifeq "$(findstring clang,$(CC))" ""
+    ifeq "$(findstring gcc,$(CC))" ""
+     ifeq "$(findstring icc,$(CC))" ""
+      # Others? try LTO
+      CFLAGS += $(LTOSYN)
+      LDFLAGS += $(LTOSYN)
+     else
+      # Intel C Compiler Classic: use IPO
+      CFLAGS += -ipo -diag-disable=10440
+      LDFLAGS += -ipo -diag-disable=10440
+     endif
+    else
+     # GCC
+     CFLAGS += $(LTOSYN)
+     LDFLAGS += $(LTOSYN)
+    endif
+   else
+    # Clang
+    CFLAGS += $(LTOSYN) -Wno-ignored-optimization-argument
+    LDFLAGS += $(LTOSYN) -Wno-ignored-optimization-argument
+   endif
+  else
+   # Oracle Studio: no LTO
+  endif
+ else
+  # NVIDIA HPC SDK: no LTO
+ endif
+else
+ # IBM XLC (V16): no LTO
+endif
+
+# Solaris or illumos: Force `-flto=auto` to `-flto`
+ifneq "$(findstring SunOS,$(shell uname -s 2> /dev/null))" ""
+ CFLAGS := $(subst -flto=auto,-flto,$(CFLAGS))
+ LDFLAGS := $(subst -flto=auto,-flto,$(LDFLAGS))
+endif
+
+# AIX with GCC: no LTO
+ifneq "$(findstring AIX,$(shell uname -s 2> /dev/null))" ""
+ ifneq "$(findstring gcc,$(CC))" ""
+  CFLAGS := $(subst -flto=auto,-flto,$(CFLAGS))
+  CFLAGS := $(subst -flto,,$(CFLAGS))
+  LDFLAGS := $(subst -flto=auto,-flto,$(LDFLAGS))
+  LDFLAGS := $(subst -flto,,$(LDFLAGS))
+ endif
+endif
+
+# OS/400 with GCC: no LTO
+ifneq "$(findstring OS400,$(shell uname -s 2> /dev/null))" ""
+ ifneq "$(findstring gcc,$(CC))" ""
+  CFLAGS := $(subst -flto=auto,-flto,$(CFLAGS))
+  CFLAGS := $(subst -flto,,$(CFLAGS))
+  LDFLAGS := $(subst -flto=auto,-flto,$(LDFLAGS))
+  LDFLAGS := $(subst -flto,,$(LDFLAGS))
+ endif
+endif
